f8c0822bb0f25a8a3e65c33c5518b7b52c749995
NewbieLearnCode2508 <trantiendat25082003@gmail.com>
2024-10-09 22:21:36 +0700

    update show san pham

--- QL_CuaHangVatLieuXayDung/GiaoDien/MenuTab/frmSanPham.cs
+++ QL_CuaHangVatLieuXayDung/GiaoDien/MenuTab/frmSanPham.cs
@@ -9,14 +9,33 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using System.Collections.ObjectModel;
+
+
 namespace GiaoDien.MenuTab
 {
     public partial class frmSanPham : Form
     {
 
+        public class SanPham
+        {
+            public string MaSanPham { get; set; }
+            public string TenSanPham { get; set; }
+            public decimal DonGia { get; set; }
+            public int SoLuongTon { get; set; }
+            public string MoTa { get; set; }
+        }
+
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<BsonDocument> collection;
+
         public frmSanPham()
         {
             InitializeComponent();
+            client = new MongoClient("mongodb://localhost:27017");
+            database = client.GetDatabase("QL_VLXD");
+            collection = database.GetCollection<BsonDocument>("SanPham");
         }
         bool checkThem;
         int indexGrid;
@@ -30,10 +49,10 @@ namespace GiaoDien.MenuTab
         }
         void enableCacControl()
         {
-            if(indexGrid != -1)
+            if (indexGrid != -1)
             {
-                btnSua.Enabled=true;
-                btnXoa.Enabled=true;
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
                 checkThem = false;
             }
         }
@@ -42,8 +61,69 @@ namespace GiaoDien.MenuTab
         {
             checkThem = false;
             voHieuHoaControl();
-
+            LoadSanPham();
         }
+
+        private void LoadSanPham()
+        {
+            try
+            {
+                var sanPhamList = new List<SanPham>();
+                var documents = collection.Find(new BsonDocument()).ToList();
+
+                foreach (var document in documents)
+                {
+                    if (document.Contains("SanPham") && document["SanPham"].IsBsonArray)
+                    {
+                        var loaiSanPham = document["SanPham"].AsBsonArray;
+                        foreach (var sp in loaiSanPham)
+                        {
+                            sanPhamList.Add(new SanPham
+                            {
+                                MaSanPham = sp["MaSanPham"].AsString,
+                                TenSanPham = sp["TenSanPham"].AsString,
+                                DonGia = ConvertToDecimal(sp["DonGia"]),
+                                SoLuongTon = sp["SoLuongTon"].ToInt32(),
+                                MoTa = sp["MoTa"].AsString
+                            });
+                        }
+                    }
+                }
+
+                gridSanPham.DataSource = sanPhamList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tải dữ liệu sản phẩm: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private decimal ConvertToDecimal(BsonValue value)
+        {
+
+            if (value.IsDecimal128)
+            {
+                decimal asDecimal128 = (decimal)value.AsDecimal128;
+                return asDecimal128;
+            }
+            else if (value.IsDouble)
+            {
+                return Convert.ToDecimal(value.AsDouble);
+            }
+            else if (value.IsInt32)
+            {
+                return Convert.ToDecimal(value.AsInt32);
+            }
+            else if (value.IsInt64)
+            {
+                return Convert.ToDecimal(value.AsInt64);
+            }
+            else
+            {
+                throw new InvalidCastException("Unable to cast the value to decimal.");
+            }
+        }
+
 
         private void gridSanPham_CellClick(object sender, DataGridViewCellEventArgs e)
         {
